dc8af60bcdb089dbd2f35b295cc45913e1a90f24
Ali C <17682708+infernoarchon@users.noreply.github.com>
2019-02-26 14:10:55 -0500

    mongodb heroku details

--- server.js
+++ server.js
@@ -27,7 +27,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/newsscraper", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newsscraper";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 mongoose.set('useCreateIndex', true)
 
 // Routes
